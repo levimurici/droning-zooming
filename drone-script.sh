740e82b675d6aad18f94b24a37c2972aab346079
levimurici <79997035+levimurici@users.noreply.github.com>
2021-07-31 08:33:42 -0300

    Update drone-script.sh
    -y adicionado

--- drone-script.sh
+++ drone-script.sh
@@ -1,16 +1,17 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install python-pip
-sudo apt-get install python-dev
-sudo pip3 install future
-sudo apt-get install screen wxgtk libxml libxslt
-sudo pip3 install numpy
-sudo pip3 install pyserial
-sudo pip3 install dronekit
-sudo pip3 install MAVProxy
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-dev
+sudo pip3 -y install future
+sudo apt-get -y install screen wxgtk libxml libxslt
+sudo pip3 -y install numpy
+sudo pip3 -y install pyserial
+sudo pip3 -y install dronekit
+sudo pip3 -y install MAVProxy
 
 echo "---- Modificar UART config em raspi-config -----"
 echo "---- em /boot/config.txt adicionar 'dtoverlay=disable-bt' ----"
 echo "---- se ttyAMA0 não está listado em /dev/, adicionar 'enable_uart=1 em /boot/config.txt'"
+sleep 3
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get -y update && sudo apt-get -y upgrade
